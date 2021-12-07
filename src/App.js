dde2080700d816bb3feb312f86704f0b03fba06a
Marcelo Magday <61895080+RagingRedBull@users.noreply.github.com>
2021-12-07 21:56:46 +0800

    Initial Commit
    React project

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
+import DetectorLog from './components/DetectorLog';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <nav className="navbar navbar-expand">
+          <div className="collapse navbar-collapse">
+            <ul className="navbar-nav me-auto ">
+              <li className="nav-item">
+                <Link to="/" className="nav-link">Home</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/detector" className="nav-link">Detectors</Link>
+              </li>
+              <li>
+                <Link to="/logs" className="nav-link">Logs</Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/floor" className="nav-link">Floors</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/logs" element={<DetectorLog />}/>
+          <Route path="/detector" element={<Detector/>}/>
+          <Route path="/floor" element={<Floors />}/>
+        </Routes>
+      </div>
+    </Router>
+
+  );
+}
+
+function Detector() {
+  return <h3>Detector</h3>
+}
+function Home() {
+  return <h3>Home</h3>
+}
+function Floors() {
+  return <h3>Floors</h3>
+}
+export default App;
