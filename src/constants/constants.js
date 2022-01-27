fa3636b1c001512817bc9a49676c4c087901548b
deosumatra <deo.sumatra@yahoo.com>
2022-01-27 12:54:05 +0800

    test

--- src/constants/constants.js
+++
@@ -1,7 +0,0 @@
-module.exports = global.config = {
-    server: {
-           url: 'IP_HERE:8080/prmts'
-        }
-        // rest of your translation object
-    // other global config variables you wish
-};
