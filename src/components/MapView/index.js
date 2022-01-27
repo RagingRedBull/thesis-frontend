1c1ffdd9057cff74bd5e235af94cb6de814d5d35
deosumatra <deo.sumatra@yahoo.com>
2022-01-27 12:12:27 +0800

    Converted fetch into axios in mapview component

--- src/components/MapView/index.js
+++ src/components/MapView/index.js
@@ -1,48 +1,48 @@
-import SideNav from '../SideNav'
-import Map from '../Map'
-import Header from '../Header'
-import { useEffect, useState } from 'react';
+import SideNav from "../SideNav";
+import Map from "../Map";
+import Header from "../Header";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const MapView = () => {
-    const [floors, setFloors] = useState([])
-    const [currentFloor, setCurrentFloor] = useState([])
+  const [floors, setFloors] = useState([]);
+  const [currentFloor, setCurrentFloor] = useState([]);
 
-    // Get floors
-    useEffect(() => {
-        const getFloors = async () => {
-            const response = await fetch("http://192.168.67.128:8080/prmts/floors")
-            var data = await response.json()
-            data = data._embedded.floors
-            
-            setFloors(data)
-        }
+  // Get floors
+  useEffect(() => {
+    const getFloors = async () => {
+      axios
+        .get("http://192.168.89.128:8080/prmts/floors")
+        .then((response) => {
+          setFloors(response.data._embedded.floors);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    };
+    getFloors();
+  });
 
-        getFloors()
-    }, [])
-
-    return (
-        <div className='container' style={ mapViewStyle }>
-            <div className='row'>
-                <div className='col'>
-                    <SideNav 
-                        floors={ floors }
-                        setCurrentFloor={ setCurrentFloor }
-                    />
-                </div>
-                <div className='col-10'>
-                    <Header />
-                    <Map image={ currentFloor.imageName } />
-                </div>
-            </div>
+  return (
+    <div className="container" style={mapViewStyle}>
+      <div className="row">
+        <div className="col">
+          <SideNav floors={floors} setCurrentFloor={setCurrentFloor} />
         </div>
-    )
+        <div className="col-10">
+          <Header />
+          <Map image={currentFloor.imageName} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // To be replaced with Tailwind
 const mapViewStyle = {
-    height: '720px',
-    backgroundColor: 'grey',
-    position: 'relative'
-}
+  height: "720px",
+  backgroundColor: "grey",
+  position: "relative",
+};
 
 export default MapView;
