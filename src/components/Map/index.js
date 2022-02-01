e2b7fa8e5252e0e6525f797b992559dd3a0bedd6
Marvin-Sy <janmartinvincent@gmail.com>
2022-02-01 20:33:09 +0800

    Updated Map features
    Included zoom function, and retrieved compartments from the database.

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,74 +1,76 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 import { Stage, Layer, Image, Rect } from 'react-konva'
-import UseImage from 'use-image'
+import useImage from 'use-image'
+import MessageBox from '../MessageBox'
 
-const index = ({ image, hasFloors }) => {
+const Map = ({ image, hasFloors, floorId }) => {
     const imageUrl = global.config.server.url + "/images/" + image
-    const [map] = UseImage(imageUrl)
+    const [map] = useImage(imageUrl)
+    const [compartments, setCompartments] = useState([])
+    const [scale, setScale] = useState(1)
 
-    // To be set in useEffect with floor foreign key.
-    const compartments = [
-        {
-            // Insert foreign key for floors
-            konvaXCoord: 102,
-            konvaYcoord: 109,
-            konvaWidth: 245,
-            konvaHeight: 167,
-            color: "white",
-            borderColor: "black",
-            // Actual compartment dimensions in meters based on CFAST.
-            xCoord: 0,
-            yCoord: 0,
-            width: 10,
-            depth: 10,
-        },
-        {
-            // Insert foreign key for floors
-            konvaXCoord: 928,
-            konvaYcoord: 109,
-            konvaWidth: 245,
-            konvaHeight: 167,
-            color: "red",
-            borderColor: "black",
-            // Actual compartment dimensions in meters based on CFAST.
-            xCoord: 0,
-            yCoord: 0,
-            width: 10,
-            depth: 10,
-        }
-    ]
+    useEffect(() => {
+        getCompartments()
+    }, [])
+
+    const getCompartments = async () => {
+        axios  
+            .get(global.config.server.url + "/floor/2/compartment") // id is currently hardcoded until it is returned.
+            .then((response) => {
+                setCompartments(response.data)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    
+    const zoom = (e) => {
+        e.evt.preventDefault()
+
+        const scaleBy = 1.02
+        const stage = e.target.getStage()
+        const oldScale = stage.scaleX()
+
+        const newScale = e.evt.deltaY > 0 ? oldScale * scaleBy : oldScale / scaleBy
+
+        setScale(newScale)
+    }
     
     return (
         <div className='m-5 d-flex justify-content-center'>
-            <div className='map-wrapper'>
-                {/* need to get the width and height of the current div */}
-                <Stage width={ 1280 } height={ 720 }>
-                    <Layer>
-                        <Image image={ map } width={ 1280 } height={ 720 } />
-                        { compartments.map((compartment, index) => (
-                            <Rect 
-                                key={ index }
-                                x={ compartment.konvaXCoord }
-                                y={ compartment.konvaYcoord }
-                                width={ compartment.konvaWidth }
-                                height={ compartment.konvaHeight }
-                                fill={ compartment.color }
-                                stroke={ compartment.borderColor }
-                                opacity={ 0.5 }
-                            />
-                        ))}
-                    </Layer>
-                </Stage>
-            </div>
-                {/* { hasFloors ? 
-                    <img 
-                        src={ imageUrl }
-                        alt='Map of the current floor'
-                        className='map'
-                    /> :
-                    <p className='p-2'>Please add a floor.</p>
-                } */}
+            {hasFloors ? 
+                <div className='map-wrapper'>
+                    {/* need to get the width and height of the current div */}
+                    <Stage 
+                        width={ 1280 } 
+                        height={ 720 } 
+                        scaleX={ scale } 
+                        scaleY={ scale }
+                        onWheel={ zoom }
+                    >
+                        <Layer>
+                            <Image image={ map } />
+                            { compartments.map((compartment, index) => (
+                                <Rect 
+                                    key={ index }
+                                    x={ compartment.xKonva }
+                                    y={ compartment.yKonva }
+                                    width={ compartment.widthKonva }
+                                    height={ compartment.heightKonva }
+                                    fill={ "white" }
+                                    stroke={ "black" }
+                                    opacity={ 0.5 }
+                                />
+                            ))}
+                        </Layer>
+                    </Stage>
+                </div>
+                :
+                <MessageBox message="Please add a floor." />
+            }
         </div>
     )
 };
 
-export default index;
+export default Map;
