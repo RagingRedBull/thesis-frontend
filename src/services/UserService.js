af0555a60a6aae87f0e4f420233f4b1efe98f5ae
deosumatra <deo.sumatra@yahoo.com>
2022-03-07 13:21:17 +0800

    Refactored the logout button so it redirects the user to '/'

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,13 +1,15 @@
 import Keycloak from "keycloak-js";
 //Customhook for user and keycloak interaction
-const _kc = new Keycloak('/keycloak.json');
+const _kc = new Keycloak("/keycloak.json");
 
 const initKeycloak = (onAuthenticatedCallback) => {
-  _kc.init({
-    onLoad: 'check-sso',
-    silentCheckSsoRedirectUri: window.location.origin + '/silent-check-sso.html',
-    pkceMethod: 'S256',
-  })
+  _kc
+    .init({
+      onLoad: "check-sso",
+      silentCheckSsoRedirectUri:
+        window.location.origin + "/silent-check-sso.html",
+      pkceMethod: "S256",
+    })
     .then((authenticated) => {
       if (!authenticated) {
         console.log("user is not authenticated..!");
@@ -19,25 +21,26 @@ const initKeycloak = (onAuthenticatedCallback) => {
 
 const doLogin = _kc.login;
 
-const doLogout = _kc.logout;
+const doLogout = () => {
+  _kc.logout({ redirectUri: 'http://localhost:3000/' });
+};
 
 const getToken = () => _kc.token;
 
 const isLoggedIn = () => {
   return !!_kc.token;
-}
+};
 
 const updateToken = (successCallback) =>
-  _kc.updateToken(500)
-    .then(successCallback)
-    .catch(doLogin);
+  _kc.updateToken(500).then(successCallback).catch(doLogin);
 
 const getUsername = () => _kc.tokenParsed?.preferred_username;
 
 const hasRole = (roles) => roles.some((role) => _kc.hasRealmRole(role));
 
 const updateToken2 = () =>
-  _kc.updateToken(1)
+  _kc
+    .updateToken(1)
     .then(() => {
       console.log("UPDATES THE TOKEN");
     })
