ac2bb04b6ef980e0df774996dfb60da86a5d9400
deosumatra <deo.sumatra@yahoo.com>
2022-03-13 19:50:50 +0800

    Changed the thresholds for the gas and fire sensors

--- src/components/SidePanel/index.js
+++ src/components/SidePanel/index.js
@@ -59,21 +59,21 @@ const SidePanel = ({ hidden, setSelectedComp, detectors, compName, compId }) => 
                             }
                         }
                         else if(sensor.type === "MQ"){
-                            if(sensor.name === "MQ-2" && sensor.mqValue > 300){
+                            if(sensor.name === "MQ-2" && sensor.mqValue > 165){
                                 isTriggered = true;
                             }
-                            else if (sensor.name === "MQ-5" && sensor.mqValue > 500){
+                            else if (sensor.name === "MQ-5" && sensor.mqValue > 455){
                                 isTriggered = true;
                             }
-                            else if (sensor.name === "MQ-7" && sensor.mqValue > 300){
+                            else if (sensor.name === "MQ-7" && sensor.mqValue > 190){
                                 isTriggered = true;
                             }
-                            else if (sensor.name === "MQ-135" && sensor.mqValue > 300){
+                            else if (sensor.name === "MQ-135" && sensor.mqValue > 210){
                                 isTriggered = true;
                             }
                         }
                         else if(sensor.type === "FIRE"){
-                            if(sensor.name === "FIRE" && sensor.sensorValue > 85){
+                            if(sensor.name === "FIRE" && sensor.sensorValue > 120){
                                 isTriggered = true;
                             }
                         }
